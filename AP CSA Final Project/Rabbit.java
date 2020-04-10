ec3a6dd0fbafec682d5f32c9cedbe8b165916fdb
Sammy Kapoor <sammykapoor10@outlook.com>
2020-04-10 18:43:35 -0400

    Create Rabbit.java

---
+++ AP CSA Final Project/Rabbit.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rabbit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rabbit extends Actor
+{
+    /**
+     * Act - do whatever the Rabbit wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (Greenfoot.isKeyDown("Up")) {
+            setRotation(-90);
+            move(5);
+        }
+        if (Greenfoot.isKeyDown("Down")) {
+            setRotation(90);
+            move(5);
+        }
+        if (Greenfoot.isKeyDown("Right")) {
+            setRotation(0);
+            move(5);
+        }
+        if (Greenfoot.isKeyDown("Left")) {
+            setRotation(180);
+            move(5);
+        }
+        
+    }    
+}
