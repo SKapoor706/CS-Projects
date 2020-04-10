3638a0fc0c7addcc7183cc8f9aa3b3d7e80e4e92
Sammy Kapoor <sammykapoor10@outlook.com>
2020-04-10 18:45:11 -0400

    Create Snake.java

---
+++ AP CSA Final Project/Snake.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Snake here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Snake extends Actor
+{
+    /**
+     * Act - do whatever the Snake wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move(1);
+        if (Greenfoot.getRandomNumber(100) < 10) {
+            turn(Greenfoot.getRandomNumber(90) - 45);
+        }
+        if (isAtEdge()) {
+            turn(180);
+        }
+    }    
+}
